c033f72ecd49814e512dec3b2622d7dde9f42b2f
Sergey <sergeyveletnyuk57@gmail.com>
2022-11-26 17:56:18 +0300

    FillArray, PrintArray, IndexOf

---
+++ lesson8_library/Program.cs
@@ -0,0 +1,50 @@
+﻿
+void FillArray(int[] colection)
+{
+    int length = colection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        colection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] colection, int find)
+{
+    int count = colection.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(colection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ int pos = IndexOf(array, 4);
+ Console.WriteLine(pos);
+
